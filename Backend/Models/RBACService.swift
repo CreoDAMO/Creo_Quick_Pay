171aa90dfd950b326e232bd125e2b9ad150c965d
Jacque Antoine DeGraff <jacquedegraff@creodamo.com>
2024-04-07 15:54:51 -0400

    Create RBACService.swift
    git add RBACService.swift

---
+++ Backend/Models/RBACService.swift
@@ -0,0 +1,8 @@
+// RBACService.swift
+import SwiftRBAC
+
+let rbac = RBAC()
+
+func checkPermission(forUser user: User, resource: String, action: String) -> Bool {
+    // Check user's permission for the given resource and action
+}
