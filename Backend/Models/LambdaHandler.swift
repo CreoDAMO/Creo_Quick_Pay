4acb953708d3babfa5e2316dd6e870bc6957e763
Jacque Antoine DeGraff <jacquedegraff@creodamo.com>
2024-04-09 01:05:41 -0400

    Update LambdaHandler.swift

--- Backend/Models/LambdaHandler.swift
+++ Backend/Models/LambdaHandler.swift
@@ -1,12 +1,16 @@
-// LambdaHandler.swift
 import AWSLambdaEvents
 import AWSLambdaRuntime
+import NIO
 
 struct LambdaHandler: EventLoopLambdaHandler {
     typealias In = AWSLambdaEvents.APIGateway.ProxyRequest
     typealias Out = AWSLambdaEvents.APIGateway.ProxyResponse
 
     func handle(context: Lambda.Context, event: In) -> EventLoopFuture<Out> {
-        // Handle Lambda event
+        // Create a response with a "Hello, World!" body.
+        let response = AWSLambdaEvents.APIGateway.ProxyResponse(statusCode: .ok, headers: ["Content-Type": "text/plain"], multiValueHeaders: nil, body: "Hello, World!", isBase64Encoded: false)
+
+        // Return the response wrapped in a future.
+        return context.eventLoop.makeSucceededFuture(response)
     }
 }
